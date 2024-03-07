d9f818703d37d9d85d4a2640a1f6a020d261ea28
kevitruon <146457163+kevitruon@users.noreply.github.com>
2024-03-06 17:58:15 -0800

    Fixing Pipeline

--- api/routers/workouts.py
+++ api/routers/workouts.py
@@ -1,11 +1,17 @@
 from fastapi import APIRouter, Depends, Response, Request
 from typing import List, Optional, Union
-from queries.workouts import WorkoutIn, WorkoutOut, WorkoutRepository, WorkoutErrorMsg
+from queries.workouts import (
+    WorkoutIn,
+    WorkoutOut,
+    WorkoutRepository,
+    WorkoutErrorMsg,
+)
 from authenticator import authenticator
 
 router = APIRouter()
 
-@router.get('/workouts', response_model=List[WorkoutOut] | WorkoutErrorMsg)
+
+@router.get("/workouts", response_model=List[WorkoutOut] | WorkoutErrorMsg)
 async def get_workouts(
     request: Request,
     account_data: dict = Depends(authenticator.get_current_account_data),
@@ -15,19 +21,16 @@ async def get_workouts(
     return repo.get_all(user_id)
 
 
-
-
-
-
-    
-
-@router.get('/workouts/{workout_id}', response_model=Optional[WorkoutOut] | WorkoutErrorMsg)
+@router.get(
+    "/workouts/{workout_id}",
+    response_model=Optional[WorkoutOut] | WorkoutErrorMsg,
+)
 async def get_workout_detail(
     workout_id: int,
     request: Request,
     response: Response,
     repo: WorkoutRepository = Depends(),
-    account_data: dict = Depends(authenticator.get_current_account_data)
+    account_data: dict = Depends(authenticator.get_current_account_data),
 ) -> Optional[WorkoutOut] | WorkoutErrorMsg:
     user_id = account_data["id"]
     result = repo.get_detail(workout_id, user_id)
@@ -40,13 +43,14 @@ async def get_workout_detail(
     else:
         return result
 
-@router.post('/workouts', response_model=WorkoutOut | WorkoutErrorMsg)
+
+@router.post("/workouts", response_model=WorkoutOut | WorkoutErrorMsg)
 async def create_workout(
     workout: WorkoutIn,
     request: Request,
     response: Response,
     repo: WorkoutRepository = Depends(),
-    account_data: dict = Depends(authenticator.get_current_account_data)
+    account_data: dict = Depends(authenticator.get_current_account_data),
 ) -> WorkoutOut | WorkoutErrorMsg:
     user_id = account_data["id"]
     created_workout = repo.create(workout, user_id)
@@ -55,16 +59,16 @@ async def create_workout(
     return created_workout
 
 
-
-
-@router.put('/workouts/{workout_id}', response_model=WorkoutOut | WorkoutErrorMsg)
+@router.put(
+    "/workouts/{workout_id}", response_model=WorkoutOut | WorkoutErrorMsg
+)
 async def update_workout(
     workout_id: int,
     workout: WorkoutIn,
     request: Request,
     response: Response,
     repo: WorkoutRepository = Depends(),
-    account_data: dict = Depends(authenticator.get_current_account_data)
+    account_data: dict = Depends(authenticator.get_current_account_data),
 ) -> WorkoutOut | WorkoutErrorMsg:
     user_id = account_data["id"]
     updated_workout = repo.update(workout_id, workout, user_id)
@@ -72,13 +76,14 @@ async def update_workout(
         response.status_code = 400
     return updated_workout
 
-@router.delete('/workouts/{workout_id}', response_model=bool)
+
+@router.delete("/workouts/{workout_id}", response_model=bool)
 async def delete_workout(
     workout_id: int,
     request: Request,
     response: Response,
     repo: WorkoutRepository = Depends(),
-    account_data: dict = Depends(authenticator.get_current_account_data)
+    account_data: dict = Depends(authenticator.get_current_account_data),
 ) -> bool:
     user_id = account_data["id"]
     success = repo.delete(workout_id, user_id)
