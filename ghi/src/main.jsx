bf44dc7da3b6c385f508fc33baf5b0defb644885
kevitruon <kevitruon@gmail.com>
2024-03-22 06:57:03 +0000

    Fixcss3

--- ghi/src/main.jsx
+++ ghi/src/main.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { AuthProvider } from '@galvanize-inc/jwtdown-for-react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
-import 'tailwindcss/tailwind.css';
+
 
 const baseUrl = import.meta.env.VITE_API_HOST
 
